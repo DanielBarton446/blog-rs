5d8e9a011dc0a4881a093205ee2faf033d56d214
Daniel Barton <daniel.barton446@gmail.com>
2024-11-14 20:40:15 -0500

    feat: content injection works now

--- src/blog/handlers/post_handler.rs
+++ src/blog/handlers/post_handler.rs
@@ -1,3 +1,5 @@
+use std::io::Error;
+
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
 
@@ -11,51 +13,79 @@ pub struct PostData {
     date: String,
 }
 
-
-
-fn extract_post_data(file_name: String) -> Option<PostData> {
-    match std::fs::read_to_string(file_name) {
-        Ok(data) => {
-            match toml::from_str::<PostData>(&data) {
-                Ok(_post_data) => {
-                    let post_data: PostData = toml::from_str(&data).unwrap();
-                    return Some(post_data);
-                }
-                Err(e) => {
-                    println!("{}", e);
-                    return None;
-                }
-            }
-        },
-        Err(_) => {
-            return None;
+fn extract_post_markdown(post_name: String, content_filename: &str) -> Result<String, Error> {
+    // convert from markdown to html?
+    // of course we can't find "content.md" here because we aren't in the correcty
+    // directory
+    match std::fs::read_to_string(format!("./posts/{}/{}", post_name, content_filename)){
+        Ok(content) => return Ok(content),
+        Err(e) => {
+            println!("{}", e); // replace me with log pls pls pls
+            println!("{}, {}", post_name, content_filename); // replace me with log pls pls pls
+            return Err(e);
         }
     }
 }
 
-
-
-#[actix_web::get("/posts/{post_name}")]
-pub async fn get_post(
-    templ: web::Data<tera::Tera>,
-    post_name: web::Path<String>) -> HttpResponse {
-    let post_name = post_name.into_inner();
-
-    let mut context = tera::Context::new(); 
-
-
-    match extract_post_data(format!("posts/{}/post_data.toml", post_name)) {
-        Some(post_meta_data) => {
-            context.insert("meta_data", &post_meta_data);
-            match templ.render("post.html", &context) {
-                Ok(content) => HttpResponse::Ok().content_type("text/html").body(content),
-                Err(e) => {
-                    println!("{}", e);  // we really should log instead of stdout
-                    HttpResponse::InternalServerError().body("Something went terribly wrong")},
+fn extract_post_metadata(file_name: String) -> Result<PostData, Error> {
+    match std::fs::read_to_string(file_name) {
+        Ok(data) => match toml::from_str::<PostData>(&data) {
+            Ok(post_data) => {
+                return Ok(post_data);
+            }
+            Err(e) => {
+                println!("{}", e); // replace me with log pls pls pls
+                return Err(Error::new(
+                    std::io::ErrorKind::Other,
+                    "Could not extract expected fields from toml",
+                ));
             }
         },
-        None => HttpResponse::NotFound().body("Not found!")
+        Err(e) => {
+            return Err(e);
+        }
+    }
+}
 
+#[actix_web::get("/posts/{post_name}")]
+pub async fn get_post(templ: web::Data<tera::Tera>, post_name: web::Path<String>) -> HttpResponse {
+    let post_name = post_name.into_inner();
+    let mut context = tera::Context::new();
+
+    match extract_post_metadata(format!("posts/{}/post_data.toml", post_name)) {
+        Ok(post_meta_data) => {
+            context.insert("meta_data", &post_meta_data);
+        }
+        Err(e) => {
+            println!("{}", e);
+            return HttpResponse::InternalServerError().into();
+        }
     }
 
+    match context.get("meta_data") {
+        Some(meta) => {
+            match meta.get("file_name").and_then(|file_name| file_name.as_str()) {
+                Some(file_name) => {
+                    // we can now extract and parse our markdown
+                    match extract_post_markdown(post_name, file_name) {
+                        Ok(content) => {
+                            context.insert("post_content", &markdown::to_html(&content));
+                        }
+                        // we don't have to have content. 500 are sad. fail softly
+                        Err(_) => println!(""),
+                    }
+                }
+                None => return HttpResponse::NoContent().into(),
+            }
+        }
+        None => return HttpResponse::NoContent().into(),
+    }
+
+    match templ.render("post.html", &context) {
+        Ok(content) => HttpResponse::Ok().content_type("text/html").body(content),
+        Err(e) => {
+            println!("{}", e); // we really should log instead of stdout
+            HttpResponse::InternalServerError().into()
+        }
+    }
 }
