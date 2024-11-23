d801d98d3f439848b1faa4247f95ed17d4968374
Daniel Barton <daniel.barton446@gmail.com>
2024-11-23 12:16:26 -0500

    feat: pastel theming

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["../templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pastel-yellow': '#FAEDCB',
+        'pastel-seafoam': '#C9E4DE',
+        'pastel-blue': '#C6DEF1',
+        'pastel-purple': '#DBCDF0',
+        'pastel-pink': '#F2C6DE',
+        'pastel-salmon': '#F7D9C4',
+      }
+    },
     listStyleType: {
       none: 'none',
       disc: 'disc',
